90cf8676b39d26678f004d4c5577c5279e1943d4
YaMang -w- <98645233+YaMang0w0@users.noreply.github.com>
2024-05-01 13:48:53 +0900

    Add files via upload

---
+++ LoLChatCleanup/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace LoLChatCleanup
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.rTeam = new System.Windows.Forms.ListBox();
+            this.bTeam = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ForeColor = System.Drawing.SystemColors.Info;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(573, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Chat Time";
+            this.columnHeader1.Width = 74;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channel";
+            this.columnHeader2.Width = 66;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Display Name";
+            this.columnHeader3.Width = 123;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Message";
+            this.columnHeader4.Width = 305;
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.openFileBtn.Location = new System.Drawing.Point(594, 12);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(190, 34);
+            this.openFileBtn.TabIndex = 1;
+            this.openFileBtn.Text = "Read File";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // rTeam
+            // 
+            this.rTeam.BackColor = System.Drawing.SystemColors.MenuText;
+            this.rTeam.ForeColor = System.Drawing.SystemColors.Info;
+            this.rTeam.FormattingEnabled = true;
+            this.rTeam.ItemHeight = 12;
+            this.rTeam.Location = new System.Drawing.Point(591, 193);
+            this.rTeam.Name = "rTeam";
+            this.rTeam.Size = new System.Drawing.Size(193, 112);
+            this.rTeam.TabIndex = 2;
+            // 
+            // bTeam
+            // 
+            this.bTeam.BackColor = System.Drawing.SystemColors.MenuText;
+            this.bTeam.ForeColor = System.Drawing.SystemColors.Info;
+            this.bTeam.FormattingEnabled = true;
+            this.bTeam.ItemHeight = 12;
+            this.bTeam.Location = new System.Drawing.Point(591, 327);
+            this.bTeam.Name = "bTeam";
+            this.bTeam.Size = new System.Drawing.Size(193, 112);
+            this.bTeam.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(591, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Red Team";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(588, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Blue Team";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(594, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Export File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(593, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Made by YaMang -w-";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bTeam);
+            this.Controls.Add(this.rTeam);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "LOL Chat CleanUp Tools (롤 채팅 정리 도구)";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.ListBox rTeam;
+        private System.Windows.Forms.ListBox bTeam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LoLChatCleanup/MainForm.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace LoLChatCleanup
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public string ShowFileOpenDialog()
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "r3dlog need a text";
+            ofd.FileName = "r3dlog";
+            ofd.Filter = "텍스트 파일 *.txt | *.txt";
+
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+
+            string path = "";
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+                path = ofd.FileName;
+            }
+            //취소버튼 클릭시 또는 ESC키로 파일창을 종료 했을경우
+            else if (dr == DialogResult.Cancel)
+            {
+                path = "";
+            }
+
+            return path;
+        }
+
+        private void ReadTextFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine(lines.Length);
+            var ownLine = lines.FirstOrDefault(x => x.Contains("**LOCAL**"));
+            var owndisplayName = Regex.Match(ownLine, @"'(.*?)'").Groups[1].Value;
+            var logstartTime = lines[0].Replace("000000.000| ALWAYS| Logging started at ", "");
+            this.Text += $" [ {owndisplayName} has Game ({logstartTime}) ]";
+            //채팅
+            foreach (string line in lines)
+            {
+                //레드팀
+                if (line.Contains("TeamChaos"))
+                {
+                    var displayName = Regex.Match(line, @"'(.*?)'").Groups[1].Value;
+                    var champ = Regex.Match(line, @"Champion\((.*?)\)").Groups[1].Value;
+                    var l_line = Regex.Match(line, @"TeamBuilderRole\((.*?)\)").Groups[1].Value;
+                    rTeam.Items.Add($"{displayName}({champ}, {l_line})");
+                }
+
+                //블루팀
+                if (line.Contains("TeamOrder"))
+                {
+                    var displayName = Regex.Match(line, @"'(.*?)'").Groups[1].Value;
+                    var champ = Regex.Match(line, @"Champion\((.*?)\)").Groups[1].Value;
+                    var l_line = Regex.Match(line, @"TeamBuilderRole\((.*?)\)").Groups[1].Value;
+                    bTeam.Items.Add($"{displayName}({champ}, {l_line})");
+                }
+
+                if (line.Contains("Chat received valid message: "))
+                {
+                    string displayName = Regex.Match(line, @"DisplayName\s(.*?)\sand").Groups[1].Value;
+                    string channel = Regex.Match(line, @"\[(.*?)\]").Groups[1].Value;
+                    string message = Regex.Match(line, @"Chat received valid message:\s(.*?)\swith speaker").Groups[1].Value;
+                    string timestamp = Regex.Match(line, @"^(.*?)\|").Groups[1].Value;
+                    DateTime startTime = DateTime.Parse(logstartTime);
+                    DateTime logTime = startTime.AddSeconds(Convert.ToDouble(timestamp));
+                    TimeSpan elapsedTime = logTime - startTime;
+                    var t = ConvertTime((int)elapsedTime.TotalSeconds);
+
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = $"{t}";
+                    lv.SubItems.Add($"{channel}");
+                    lv.SubItems.Add($"{displayName}");
+                    lv.SubItems.Add($"{message}");
+
+                    listView1.Items.Add(lv);
+                }
+
+                
+
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            rTeam.Items.Clear();
+            bTeam.Items.Clear();
+
+            var path = ShowFileOpenDialog();
+            Console.WriteLine(path);
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Cancel");
+                return;
+            }
+
+            ReadTextFile(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Please specify the storage path (경로를 지정해주세요)";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.Filter = "Text File (*.txt) | *.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"Timestamp | Channel | DisplayName | Message");
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    var item = listView1.Items[i];
+
+                    sb.AppendLine($"{item.Text} | {item.SubItems[1].Text} | {item.SubItems[2].Text} | {item.SubItems[3].Text}");
+                }
+                sb.AppendLine("----------------------------------------------------");
+                sb.AppendLine("Red Team");
+                for (int i = 0; i < rTeam.Items.Count; i++)
+                {
+                    sb.AppendLine($"{rTeam.Items[i]}");
+                }
+                sb.AppendLine("----------------------------------------------------");
+                sb.AppendLine("Blue Team");
+                for (int i = 0; i < bTeam.Items.Count; i++)
+                {
+                    sb.AppendLine($"{bTeam.Items[i]}");
+                }
+                sb.AppendLine("----------------------------------------------------");
+
+                File.WriteAllText(fileName, sb.ToString());
+            }
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Cancel Save");
+            }
+        }
+
+        private string ConvertTime(int seconds)
+        {
+            seconds = seconds - 42;
+
+            int convertedHours = seconds / 3600;
+            int convertedMinutes = (seconds % 3600) / 60;
+            int convertedSeconds = seconds % 60;
+
+            string timeString = "";
+
+            if (convertedHours != 0)
+            {
+                timeString += $"{convertedHours:D2}:";
+            }
+
+            if (convertedMinutes != 0 || convertedHours == 0)
+            {
+                timeString += $"{convertedMinutes:D2}:";
+            }
+
+            timeString += $"{convertedSeconds:D2}";
+
+            return timeString;
+        }
+    }
+}
